b0b84180fc4581b698855dace222dbe8a83590d2
zakaria <108371873+zikossss@users.noreply.github.com>
2026-01-22 14:42:35 +0100

    Reorder readline includes in minishell.h

--- minishell.h
+++ minishell.h
@@ -16,14 +16,14 @@
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/stat.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 
 typedef struct s_mini
 {
